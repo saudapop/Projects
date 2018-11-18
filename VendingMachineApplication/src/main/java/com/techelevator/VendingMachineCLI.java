fcf9296da02eaa0ba442ca82c633100e7ef8b811
saudahmed-dev <45129081+saudahmed-dev@users.noreply.github.com>
2018-11-17 23:49:11 -0500

    Add files via upload

---
+++ VendingMachineApplication/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -0,0 +1,44 @@
+package com.techelevator;
+
+import java.io.IOException;
+
+import com.techelevator.view.Menu;
+
+public class VendingMachineCLI {
+
+	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
+	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
+	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
+
+	private Menu menu;
+	private Inventory inventory;
+
+	public VendingMachineCLI(Menu menu) {
+		this.menu = menu;
+	}
+
+	public static void main(String[] args) throws IOException {
+		Menu menu = new Menu(System.in, System.out);
+		VendingMachineCLI cli = new VendingMachineCLI(menu);
+
+		cli.run();
+	}
+
+	public void run() throws IOException {
+
+		while (true) {
+
+			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
+
+			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
+				menu.DisplayProductMenu();
+
+			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
+				menu.DisplayProductMenu();
+				menu.DisplayPurchaseMenu();
+				
+			}
+		}
+	}
+
+}
